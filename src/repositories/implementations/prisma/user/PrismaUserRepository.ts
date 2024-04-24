6fecec7160c517224a04ac8d0e1251139c64c897
ocsoares <casoares016@gmail.com>
2024-04-24 19:53:07 -0300

    feat: add PrismaService

--- src/repositories/implementations/prisma/user/PrismaUserRepository.ts
+++ src/repositories/implementations/prisma/user/PrismaUserRepository.ts
@@ -3,8 +3,7 @@ import { PrismaService } from '../prisma-client.service';
 import { UserEntity } from '../../../../entity/UserEntity';
 
 export class PrismaUserRepository implements UserRepository {
-    // VER um jeito de pegar a Instância aqui no Constructor !!!
-    constructor(private readonly prismaService: PrismaService) {}
+    private readonly prismaService = PrismaService.getClient();
 
     async create(data: UserEntity): Promise<UserEntity> {
         const createUser = await this.prismaService.user.create({ data });
