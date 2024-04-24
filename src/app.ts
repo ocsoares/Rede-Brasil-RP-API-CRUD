e1768b71e9336ed37bf20bfd8c0f98da0d2bafe9
ocsoares <casoares016@gmail.com>
2024-04-24 19:54:30 -0300

    fix: fix to start server

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import * as swaggerUi from 'swagger-ui-express';
 import { pageNotFoundMiddleware } from './middleware/page-not-found.middleware';
 import 'express-async-errors';
 import helmet from 'helmet';
+import exampleRoute from './routes/example.route';
 
 const app: Express = express();
 
@@ -16,26 +17,18 @@ app.use(cors());
 
 app.use(helmet());
 
-// Middlewares
 app.use(morganMiddleware);
 
-// Initial route (/) redirect to Documentation Route
 app.get('/', (req: Request, res: Response): void => {
     return res.redirect('/api/docs');
 });
 
-// Documentation Route
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 
-// Padroniza TODAS as Rotas para conter /api/... na URL !! <<
-app.use(
-    '/api/',
-    // Rota exportada
-);
+app.use('/api/', exampleRoute);
 
 app.use(pageNotFoundMiddleware);
 
-// Para Funções ASSÍNCRONAS (async) PRECISA usar a lib 'express-async-errors' !! <<
 app.use(exceptionHandlerMiddleware);
 
 export { app };
--- src/config/logs.ts
+++ src/config/logs.ts
@@ -1,8 +1,5 @@
 import winston, { format, transports } from 'winston';
 
-// OBS: Como o Winston é baseado em nívels, um Log SUPERIOR pode estar CONTIDO em Outro log de nível INFERIOR, exemplo, um log de error em um
-// log de warn (PADRÃO no mundo...) !! <<
-
 const levels = {
     error: 0,
     warn: 1,
@@ -11,7 +8,6 @@ const levels = {
     debug: 4,
 };
 
-// Dependendo do Ambiente, vai lançar AVISOS ou ERROS !! <<
 const checkEnvironment = () => {
     if (process.env.NODE_ENV === 'production') {
         return 'warn';
@@ -28,10 +24,8 @@ const colorsLog = {
     debug: 'white',
 };
 
-// Adiciona as Cores passadas no Objeto acima !! <<
 winston.addColors(colorsLog);
 
-// Formatando o log com um timestamp no Padrão AMERICANO !! (Ano-mês-dia, hora:minuto:segundo) <<
 const formatLog = format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.colorize({ all: true }),
@@ -40,11 +34,9 @@ const formatLog = format.combine(
     ),
 );
 
-// É um ARRAY !! <<
 const transportsLog = [
     new transports.Console(),
 
-    // Vai criar um Arquivo para UM Determinado Log !! <<
     new transports.File({
         filename: 'logs/error.log',
         level: 'error',
@@ -70,14 +62,11 @@ const transportsLog = [
         level: 'debug',
     }),
 
-    // Vai criar um Arquivo para TODOS os LOGS !! <<
     new transports.File({
         filename: 'logs/all-logs.log',
     }),
 ];
 
-// Esse vai ser o Comando usado para Determinar o Log !! <<
-// OBS: Exportar !! <<
 const Logger = winston.createLogger({
     level: checkEnvironment(),
     levels,
