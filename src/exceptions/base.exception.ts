e20385700e4d2fb2ad654d0aa677a6cfdf165506
ocsoares <casoares016@gmail.com>
2024-04-24 17:20:10 -0300

    feat: add exceptions

---
+++ src/exceptions/base.exception.ts
@@ -0,0 +1,9 @@
+export class BaseException extends Error {
+    public readonly statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+
+        this.statusCode = statusCode;
+    }
+}
--- src/exceptions/user/user-already-exists-by-email.exception.ts
+++ src/exceptions/user/user-already-exists-by-email.exception.ts
@@ -1,26 +1,12 @@
-// TENTAR fazer igual no NestJS, mas usando os Erros do "HttpException" ou algo assim para "extends" !!!
+import { BaseException } from '../base.exception';
 
-import { BadRequestErrorMessages } from '../../@types/errorAPIMessages';
+export class UserAlreadyExistsByEmailException extends BaseException {
+    static readonly EXCEPTION_MESSAGE =
+        'There is already a user registered with this email !';
 
-export class ErrorAPIHelper extends Error {
-    public readonly statusCode: number;
+    constructor() {
+        super(UserAlreadyExistsByEmailException.EXCEPTION_MESSAGE, 400);
 
-    constructor(message: string, statusCode: number) {
-        super(message);
-
-        this.statusCode = statusCode;
+        this.name = 'UserAlreadyExistsByEmailException';
     }
 }
-
-export class BadRequestAPIError extends ErrorAPIHelper {
-    // O statusCode NÃO entrou no constructor porque, nesse caso, ele vai
-    // ser FIXO, e a message vai ser passada como Parâmetro !! <<
-
-    constructor(message: BadRequestErrorMessages) {
-        super(message, 400);
-
-        this.name = 'BadRequestError';
-    }
-}
-
-// more errors...
