8b46c7150d16ff886ec426587fc0327ad55e021a
ocsoares <casoares016@gmail.com>
2024-04-25 19:34:37 -0300

    refactor: remove duplicate UserMapper

--- src/mapper/UserMapper.ts
+++
@@ -1,8 +0,0 @@
-import { UserEntity } from "src/entity/UserEntity";
-import { UserResponse } from "src/use-cases/user/responses/UserResponse";
-
-export class UserMapper {
-    toResponse(userEntity: UserEntity): UserResponse {
-        return new UserResponse(userEntity.fullName, userEntity.email);
-    }
-}
