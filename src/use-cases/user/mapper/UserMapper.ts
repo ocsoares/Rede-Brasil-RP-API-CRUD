30d0d91c35598366b6684b2dc401e9dedc47bf0b
ocsoares <casoares016@gmail.com>
2024-04-25 13:29:17 -0300

    feat: add UserMapper with toResponse method

---
+++ src/use-cases/user/mapper/UserMapper.ts
@@ -0,0 +1,8 @@
+import { UserEntity } from 'src/entity/UserEntity';
+import { UserResponse } from 'src/use-cases/user/responses/UserResponse';
+
+export class UserMapper {
+    toResponse(userEntity: UserEntity): UserResponse {
+        return new UserResponse(userEntity.fullName, userEntity.email);
+    }
+}
